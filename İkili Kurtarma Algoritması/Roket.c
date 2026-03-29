a8b5791bf9b9b533c98ebfbe816baf906dfc99a8
TheLastt5 <mre_ylmz@outlook.com>
2026-03-29 15:20:54 +0300

    Update Roket.c

--- İkili Kurtarma Algoritması/Roket.c
+++ İkili Kurtarma Algoritması/Roket.c
@@ -1,1 +1,62 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "bmp180_for_stm32_hal.h"
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define HEDEF_IRTIFA       8000.0  // Mert dedi
+#define ANA_PARASUT_HEDEF  2000.0  // Mert dedi
+#define APOGEE_ESIK_DEGERI 5.0 // Doğruluktan emin olmak için 5 metre düşüş istiyorum 
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+I2C_HandleTypeDef hi2c1;
+
+TIM_HandleTypeDef htim6;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+uint32_t pot_degeri = 0;
+float ucus_sicaklik = 0;
+float ucus_basinc = 0;
+float ucus_irtifa = 0;
+float yer_basinci = 0.0;
 
